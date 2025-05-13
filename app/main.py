5acfcae60ec1b65eb174c0ecb789e0f986aa5c5c
luanfred <luanmatheussf4@gmail.com>
2025-05-12 21:05:20 -0300

    feat: adicionar porta 4173 no CORS

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:4173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
